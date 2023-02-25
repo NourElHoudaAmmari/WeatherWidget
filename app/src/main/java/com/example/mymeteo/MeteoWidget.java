077cd49b39739d48751ef8ee2664eae1b0bec92c
Nour El Houda <nourammari15@gmail.com>
2023-02-25 16:13:15 +0100

    final commit

--- app/src/main/java/com/example/mymeteo/MeteoWidget.java
+++ app/src/main/java/com/example/mymeteo/MeteoWidget.java
@@ -4,34 +4,39 @@ import static android.content.Context.MODE_APPEND;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
+import android.widget.RelativeLayout;
 import android.widget.RemoteViews;
 
 
 public class MeteoWidget extends AppWidgetProvider {
-
-
-
-
-
-
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
         super.onUpdate(context, appWidgetManager, appWidgetIds);
 
         for (int appWidgetId : appWidgetIds) {
 
-            // Get the layout for the App Widget
+           Intent launchAppIntent = new Intent(context, MainActivity.class);
+          PendingIntent pendingIntent ;
+          if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S){
+              pendingIntent = PendingIntent.getActivity(context, 0, launchAppIntent, PendingIntent.FLAG_UPDATE_CURRENT|PendingIntent.FLAG_MUTABLE);
+          }else{
+              pendingIntent = PendingIntent.getActivity(context, 0, launchAppIntent, PendingIntent.FLAG_UPDATE_CURRENT|PendingIntent.FLAG_IMMUTABLE
+              );
+          }
+          // Set the click listener for the widget
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.meteo_widget);
+            views.setOnClickPendingIntent(R.id.widget_layout, pendingIntent);
+            // Get the layout for the App Widget
+          //  RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.meteo_widget);
 
             // Get the current location and temperature
             // Get the current location and temperature from MainActivity
-
-
-
-
             SharedPreferences sh = context.getSharedPreferences("MySharedPref", Context.MODE_PRIVATE);
 
             // The value will be default as empty string because for the very
@@ -44,9 +49,6 @@ public class MeteoWidget extends AppWidgetProvider {
 
              String location = l1;
              String temperature = t1;
-
-
-
 
             // Update the views with the current location and temperature
             views.setTextViewText(R.id.idTVCity, l1);
--- app/src/main/java/com/example/mymeteo/MeteoWidgetReceiver.java
+++ app/src/main/java/com/example/mymeteo/MeteoWidgetReceiver.java
@@ -43,4 +43,5 @@ public class MeteoWidgetReceiver extends BroadcastReceiver {
             }
         }
     }
+
 }
